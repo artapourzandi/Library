53f3b9bc00a541a69d69538d539e5fa2342faa89
arta <artapourzandi@gmail.com>
2021-06-08 16:29:05 +0200

    everythings changed June 8

--- main.js
+++ main.js
@@ -1,55 +1,97 @@
-let myLibrary = [];
-
-function Book(title, author, pages, read){
-    this.title = title
-    this.author = author 
-    this.pages = pages
-    this.read = read
-    this.info = function() {
-        return (title + " by " + author + ', ' + "pages: " + pages + ', ' + read);
+//  Create a Book Object
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        /* this.info = function() {
+            return (title + " by " + author + ', ' + "pages: " + pages + ', ' + read);
+        } */
     }
 }
 
+// Display Tasks
+class UI {
+    static display() {
+        const myLibrary = [
+            {
+                title: 'Arta', 
+                author: 'Love', 
+                pages: 250, 
+                read: 'Not Yet'
+            }, 
+            {
+                title: 'Nasi', 
+                author: 'kindness', 
+                pages: 114, 
+                read: 'read'
+            }
+        ];
 
-// const book1 = new Book('\" The Habits', 'J K Rolling', 400, 'not read yet \"');
+        const books = myLibrary;
 
+        books.forEach((book) => UI.addBookToList(book))
+    }
 
-let values = [];
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list');
+        const row = document.createElement('tr');
 
-function addBookToLibrary() {
-    let title = document.getElementById("title").value;
-    let author = document.getElementById("author").value;
-    let pages = document.getElementById("pages").value;
-    let read = document.getElementById("read").value;
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.pages}</td>
+        <td>${book.read}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delet">Delet</td>
+        `;
 
-    let book2 = new Book(title, author, pages, read);
-    
-    // values = [title, author, pages, read];
+        list.appendChild(row);
+    }
 
-    myLibrary = myLibrary.concat(book2);
+    static deletTheRow(element) {
+        if(element.classList.contains('delet')) {
+            element.parentElement.parentElement.remove();
+        }
+    }
 
-    let btnClear = document.querySelector('button');
-    let inputs = document.querySelectorAll('input');
-
-    btnClear.addEventListener('click', () => {
-        inputs.forEach(input => input.value = '')
-    })
+    static emptyInput() {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#pages').value = '';
+        document.querySelector('#read').value = '';
+    }
 }
+ 
+
+// Storage Tasks
 
 
+// Event = Add
+document.addEventListener('DOMContentLoaded', UI.display);
+document.querySelector('#main-form').addEventListener('submit', (e) => 
+{
+
+    // we prevent default action of submit button
+    e.preventDefault();
+    // collecting the values of the form
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    const read = document.querySelector('#read').value;
+
+    // create an instance from object 'Book'
+    const book = new Book(title, author, pages, read);
+
+    // Now we add our instance to our Display
+    UI.addBookToList(book);
+
+    UI.emptyInput();
+});
 
 
-for (var i = 0; i < myLibrary.length; i++) {
-    console.table(myLibrary[i]);
-}
-
-
-
-
-
-
-
-
-
-
-// console.log(book1.info());
+// Event = Remove
+document.querySelector('#book-list').addEventListener('click', (e) => 
+{
+    UI.deletTheRow(e.target)
+});
